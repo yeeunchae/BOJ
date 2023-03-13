dfb56b8e95dcb79af290a5289cc197f95c80958c
yeeunchae <120078342+yeeunchae@users.noreply.github.com>
2023-03-13 12:59:50 +0900

    [Bronze V] Title: 고양이, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.c
@@ -0,0 +1,14 @@
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|    
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+    int main(void){
+        printf("\\    /\\\n");
+        printf(" )  ( \')\n");
+        printf("(  /  )\n");
+        printf(" \\(__)|\n");
+        
+        return 0;
+    }
