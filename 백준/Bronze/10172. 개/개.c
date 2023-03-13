db273c38a8e48658849fc13639109f0fcf0f994c
yeeunchae <120078342+yeeunchae@users.noreply.github.com>
2023-03-13 13:00:01 +0900

    [Bronze V] Title: 개, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main(void){
+    printf("|\\_/|\n");
+    printf("|q p|   /}\n");
+    printf("( 0 )\"\"\"\\\n");
+    printf("|\"^\"`    |\n");
+    printf("||_/=\\\\__|\n");
+    return 0;
+}
+
