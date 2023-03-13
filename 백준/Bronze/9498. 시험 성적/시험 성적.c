58566fca98f4879c8485cf6ac5405df05e807157
yeeunchae <120078342+yeeunchae@users.noreply.github.com>
2023-03-13 12:59:34 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+    int score;
+    scanf("%d",&score);
+    if (score >= 90 && score <=100)
+        printf("A\n");
+    else if(score >= 80 && score < 90)
+        printf("B\n");
+    else if(score >= 70 && score < 80)
+        printf("C\n");
+    else if(score >= 60 && score < 70)
+        printf("D\n");
+    else
+        printf("F\n");
+    return 0;
+}
